4ffb8c3760c87edf817deade538ec1318a1f8f71
João Paulo <jpjoao1001@gmail.com>
2021-04-25 10:01:28 -0300

    style(header): header component created

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Header />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import Header from "./components/Header.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-})
+    Header,
+  },
+});
 </script>
 
 <style>
@@ -22,6 +21,5 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <img src="../assets/logo.png" />
+    <h1>Todo</h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "Header",
+});
+</script>
+
+<style scoped>
+header {
+  background-color: white;
+  height: 60px;
+  border-radius: 10px;
+  box-shadow: 5px 8px 24px #ababab50;
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+
+header img {
+  width: 20px;
+  height: 20px;
+  margin-left: 20px;
+}
+
+header h1 {
+  font-size: 20px;
+  margin-left: 20px;
+}
+</style>
